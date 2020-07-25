8fbbc3fb8cd2fbeb78f144ff1aa2edbf47ea8788
yichienchen <pupss91325@gmail.com>
2020-07-25 13:36:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/coupon_forwarding_system/DBHelper.java
@@ -0,0 +1,34 @@
+package com.example.coupon_forwarding_system;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    final static String TB1="coupon";
+
+    private final static int VS=1;
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, null, VS);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL1 = "CREATE TABLE IF NOT EXISTS "+ TB1 +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                "NAME TEXT NOT NULL, " +
+                "PHONE TEXT , "+
+                "EMAIL TEXT , "+
+                "COMPANY TEXT , "+
+                "POSITION TEXT , "+
+                "OTHER TEXT  )";
+
+        db.execSQL(SQL1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String SQL = "DROP TABLE " + TB1;
+        db.execSQL(SQL);
+    }
+}
